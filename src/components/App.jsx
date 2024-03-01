bfbc75688ffd85135fc32a4dbd87afc830d2a76c
Iryna Husieva <02guseva02@gmail.com>
2024-03-01 17:59:16 +0100

    initial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './header/Header';
+import CartPage from '../pages/cartPage/CartPage';
+import ShopPage from '../pages/shopPage/ShopPage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <div>
+        <Header />
+        
+        <Routes>
+          <Route path="/shop" element={<ShopPage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
