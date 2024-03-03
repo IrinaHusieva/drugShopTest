bddb121f2d62a7b2b9934e4408810e8e1b62dcf7
Iryna Husieva <02guseva02@gmail.com>
2024-03-03 12:25:35 +0100

    api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './header/Header';
 import CartPage from '../pages/cartPage/CartPage';
 import ShopPage from '../pages/shopPage/ShopPage';
@@ -9,8 +9,8 @@ export const App = () => {
     <Router>
       <div>
         <Header />
-        
         <Routes>
+         <Route path="/" element={<Navigate to="/shop" />} />
           <Route path="/shop" element={<ShopPage />} />
           <Route path="/cart" element={<CartPage />} />
         </Routes>
